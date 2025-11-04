a94d67c1ee7a30ec11174d63684b27a5fd9109b2
2022184032-cmyk <2022184032@tukorea.ac.kr>
2025-11-04 20:36:10 +0900

    새의 이동속도 추가

--- bird.py
+++ bird.py
@@ -1,3 +1,10 @@
 from pico2d import *
 import game_world
 import game_framework
+
+PIXEL_PER_METER = (10.0 / 0.3)
+
+FLY_SPEED_KMPH = 60.0  # 새 비행 속도 (km/h)
+FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
+FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
